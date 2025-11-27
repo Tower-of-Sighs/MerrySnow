87dc8458d2bff110cafee9ba42a5362631a76518
Mafuyu404 <404@mafuyu.link>
2025-11-27 20:14:44 +0800

    小改

--- src/main/java/com/sighs/merrysnow/mixin/BiomeMixin.java
+++ src/main/java/com/sighs/merrysnow/mixin/BiomeMixin.java
@@ -9,10 +9,19 @@ import net.minecraft.world.level.biome.Biome;
 import net.minecraftforge.registries.ForgeRegistries;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = Biome.class)
 public class BiomeMixin {
+    @Inject(method = "getPrecipitationAt", at = @At("HEAD"), cancellable = true)
+    private void noWeather(BlockPos p_265163_, CallbackInfoReturnable<Biome.Precipitation> cir) {
+        String id = Utils.getBiomeId(Minecraft.getInstance().level, (Biome) (Object) this);
+        String result = Config.getWeatherModify(id);
+        if (result.equals("none")) cir.setReturnValue(Biome.Precipitation.NONE);
+    }
+
     @Redirect(method = "getPrecipitationAt", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/biome/Biome;coldEnoughToSnow(Lnet/minecraft/core/BlockPos;)Z"))
     private boolean customSnowWeather(Biome biome, BlockPos blockPos) {
         if (Config.ENFORCE_SNOW_WEATHER.get().equals("default")) {
