c9bb440419c2fc7dae44ecef76ad4026337175ba
Flechazo <2558755403@qq.com>
2025-12-01 17:32:31 +0800

    fix

--- src/main/java/com/sighs/merrysnow/mixin/PowderSnowBlockMixin.java
+++ src/main/java/com/sighs/merrysnow/mixin/PowderSnowBlockMixin.java
@@ -2,11 +2,13 @@ package com.sighs.merrysnow.mixin;
 
 import com.sighs.merrysnow.Config;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.PowderSnowBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -25,5 +27,18 @@ public class PowderSnowBlockMixin {
         }
         if (!enabled) return;
         cir.setReturnValue(Block.box(0.0D, 0.0D, 0.0D, 16.0D, 8.0D, 16.0D));
+    }
+
+    @Inject(method = "getEntityInsideCollisionShape", at = @At("HEAD"), cancellable = true)
+    private void modifyEntityInsideCollisionShape(BlockState state, BlockGetter level, BlockPos pos, Entity entity, CallbackInfoReturnable<VoxelShape> cir) {
+        boolean enabled;
+        try {
+            enabled = Config.MERRY_POWDER_SNOW.get();
+        } catch (IllegalStateException e) {
+            enabled = true;
+        }
+        if (!enabled) return;
+
+        cir.setReturnValue(Shapes.block());
     }
 }
